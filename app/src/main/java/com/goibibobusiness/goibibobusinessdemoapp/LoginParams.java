5cf18e1438c5d124a714ab7b287d08bbb9a19825
AbrahamV <abraham.varricatt@googlemail.com>
2016-09-27 02:36:53 +0530

    all params done

--- app/src/main/java/com/goibibobusiness/goibibobusinessdemoapp/LoginParams.java
+++ app/src/main/java/com/goibibobusiness/goibibobusinessdemoapp/LoginParams.java
@@ -1,6 +1,8 @@
 package com.goibibobusiness.goibibobusinessdemoapp;
 
 import android.content.SharedPreferences;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +10,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Map;
+import java.util.TimeZone;
 
 public class LoginParams extends AppCompatActivity {
 
@@ -23,7 +31,11 @@ public class LoginParams extends AppCompatActivity {
     }
 
     /** Making preference fragment */
-    public static class SettingsFragment extends PreferenceFragment {
+    public static class SettingsFragment extends PreferenceFragment
+            implements SharedPreferences.OnSharedPreferenceChangeListener{
+
+        SharedPreferences sharedPreferences;
+
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -31,6 +43,65 @@ public class LoginParams extends AppCompatActivity {
             // Load preferences from XML resource
             addPreferencesFromResource(R.xml.preferences);
         }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+
+            sharedPreferences = getPreferenceManager().getSharedPreferences();
+
+            // we want to watch the preference values' changes
+            sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+
+
+            Map<String, ?> preferencesMap = sharedPreferences.getAll();
+            // iterate through the preference entries and update their summary if they are an instance of EditTextPreference
+            for (Map.Entry<String, ?> preferenceEntry : preferencesMap.entrySet()) {
+//                Log.v("goibibobusinessdemoapp", "-----------" + preferenceEntry.getKey());
+                Preference pref = findPreference(preferenceEntry.getKey());
+                if (preferenceEntry.getKey().equals("time_stamp")) {
+                    // Need to update timestamp
+                    Calendar calendar = Calendar.getInstance();
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+                    Date currenTimeZone = (Date) calendar.getTime();
+                    String time_now = sdf.format(currenTimeZone);
+
+                    // updating timestamp
+                    EditTextPreference edit_pref = (EditTextPreference) pref;
+                    pref.setSummary(time_now);
+                    edit_pref.setText(time_now);
+                } else if (pref instanceof EditTextPreference) {
+                    EditTextPreference edit_pref = (EditTextPreference) pref;
+                    pref.setSummary(edit_pref.getText());
+                }
+            }
+        }
+
+        @Override
+        public void onPause() {
+            sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
+            super.onPause();
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+                                              String key) {
+//            Log.v("goibibobusinessdemoapp", "edit==============================");
+
+            Preference pref = findPreference(key);
+
+            if (pref instanceof EditTextPreference) {
+                EditTextPreference edit_pref = (EditTextPreference) pref;
+                pref.setSummary(edit_pref.getText());
+            }
+
+        }
+
+        private void updateSummary(EditTextPreference preference) {
+            // set the EditTextPreference's summary value to its current text
+            preference.setSummary(preference.getText());
+        }
+
     }
 
 
--- app/src/main/java/com/goibibobusiness/goibibobusinessdemoapp/MainActivity.java
+++ app/src/main/java/com/goibibobusiness/goibibobusinessdemoapp/MainActivity.java
@@ -2,12 +2,15 @@ package com.goibibobusiness.goibibobusinessdemoapp;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+import java.util.zip.CRC32;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,8 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         editText = (EditText) findViewById(R.id.editText_weblink);
-        updateLoginURL();
-        Log.v("goibibobusinessdemoapp", "STARTING APP----------------->>>>>>>>>>>>>");
+//        Log.v("goibibobusinessdemoapp", "STARTING APP----------------->>>>>>>>>>>>>");
     }
 
     @Override
@@ -48,6 +50,25 @@ public class MainActivity extends AppCompatActivity {
     public void updateLoginURL() {
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
         Boolean target_testing = sharedPref.getBoolean("target_testing_env", true);
+        String url_endpoint = sharedPref.getString("target_endpoint", "start_login");
+        String partner_id = sharedPref.getString("partner_id", "example_bank");
+        String userid = sharedPref.getString("user_id", "user345");
+        String timestamp = sharedPref.getString("time_stamp", "2015-06-17 14:30");
+        String token = sharedPref.getString("token", "0000000");
+        String checksum_key = sharedPref.getString("checksum_key", "testkey");
+        String api_version = sharedPref.getString("api_version", "1.0");
+        String vertical = sharedPref.getString("vertical", "flight");
+        String f_name = sharedPref.getString("f_name", "Sherlock");
+        String m_name = sharedPref.getString("m_name", "");
+        String l_name = sharedPref.getString("l_name", "Holmes");
+        String mobile = sharedPref.getString("mobile", "8877743215");
+        String email = sharedPref.getString("email", "sherlock@guerrillamail.com");
+
+        String crc_string = partner_id + userid + timestamp + token + checksum_key;
+        CRC32 crc = new CRC32();
+        crc.update(crc_string.getBytes());
+        String enc = Long.toHexString(crc.getValue());
+
 
         String prefix;
         if (target_testing) {
@@ -56,9 +77,33 @@ public class MainActivity extends AppCompatActivity {
             prefix = "https://www.";
         }
 
+        String BASE_URL = prefix + "goibibobusiness.com/b2b_partner/" + partner_id + "/" + url_endpoint + "/?";
 
-//        editText.setText(prefix + "goibibobusiness.com/b2b_partner/example_bank/start_login/");
-        editText.setText(prefix + "goibibobusiness.com/b2b_partner/");
+        Uri buildUri;
+        if (api_version.equals("1.0")) {
+            buildUri = Uri.parse(BASE_URL)
+                    .buildUpon()
+                    .appendQueryParameter("user_id", userid)
+                    .appendQueryParameter("timestamp", timestamp)
+                    .appendQueryParameter("token", token)
+                    .appendQueryParameter("checksum", enc)
+                    .appendQueryParameter("version", api_version)
+                    .appendQueryParameter("vertical", vertical)
+                    .appendQueryParameter("f_name", f_name)
+                    .appendQueryParameter("m_name", m_name)
+                    .appendQueryParameter("l_name", l_name)
+                    .appendQueryParameter("mobile", mobile)
+                    .appendQueryParameter("email", email)
+                    .build();
+        } else {
+            buildUri = Uri.parse(BASE_URL)
+                    .buildUpon()
+                    .build();
+        }
+
+        String url_string = buildUri.toString();
+
+        editText.setText(url_string);
     }
 
 }
